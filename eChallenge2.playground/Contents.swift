da882957f27c5cad937dadca0cc8249aa5ea2944
Don Johnson <djohnson@solstice-mobile.com>
2016-02-24 20:05:04 -0600

    Remove duplicate values of 3 and 5 such as 15

--- eChallenge2.playground/Contents.swift
+++ eChallenge2.playground/Contents.swift
@@ -1,12 +1,10 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
-
-var multiplesOf5: Int {
+var sumOfMultiplesOf3and5: Int {
     get {
         var sum: Int = 0;
-        for var i = 0; i < 1000; i = i+5  {
-            if i % 5 == 0 {
+        for i in 0..<1000  {
+            if i % 3 == 0 || i % 5 == 0 {
                 sum+=i
             }
         }
@@ -14,18 +12,4 @@ var multiplesOf5: Int {
     }
 }
 
-var multiplesOf3: Int {
-get {
-    var sum: Int = 0;
-    for var i = 0; i < 1000; i = i+3  {
-        if i % 3 == 0 {
-            sum += i
-        }
-    }
-    return sum
-}
-}
-
-print(multiplesOf5 + multiplesOf3)
-
-
+print(sumOfMultiplesOf3and5) // 233,168
